4b68ffed1b6ae12525976d6d1e0377f89ee28151
anadornas <analuizadornas94@gmail.com>
2025-10-13 13:46:40 +0100

    Implementa função para ocupar um quarto

--- quartos.py
+++ quartos.py
@@ -1,3 +1,7 @@
 quartos = {101: {"status": "disponivel"}, 102: {"status": "disponivel"}}
 def verificar_disponibilidade(numero):
     return quartos.get(numero, {}).get("status") == "disponivel"
+def ocupar_quarto(numero):
+    if verificar_disponibilidade(numero):
+        quartos[numero]["status"] = "ocupado"
+        print(f"Quarto {numero} ocupado.")
