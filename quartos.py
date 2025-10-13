5b92671dea3cfdab7f4891ef9cb49079386015db
anadornas <analuizadornas94@gmail.com>
2025-10-13 16:06:37 +0100

    Modifica listar_quartos para incluir 'Status do quarto' em main

--- quartos.py
+++ quartos.py
@@ -1,14 +1,19 @@
+
 quartos = {101: {"status": "disponivel"}, 102: {"status": "disponivel"}}
+
 def verificar_disponibilidade(numero):
     return quartos.get(numero, {}).get("status") == "disponivel"
+
 def ocupar_quarto(numero):
     if verificar_disponibilidade(numero):
         quartos[numero]["status"] = "ocupado"
         print(f"Quarto {numero} ocupado.")
+
 def liberar_quarto(numero):
     if numero in quartos:
         quartos[numero]["status"] = "disponivel"
         print(f"Quarto {numero} liberado.")
+
 def listar_quartos():
     for numero, info in quartos.items():
-        print(f"Quarto {numero}: {info['status']}")
+        print(f"Status do quarto {numero}: {info['status']}}
