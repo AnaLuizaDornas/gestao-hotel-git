68397742d080d7fd95548b04ff1093f8c41d20ef
anadornas <analuizadornas94@gmail.com>
2025-10-13 11:25:20 +0100

    Implementa função para registrar novo cliente

--- clientes.py
+++ clientes.py
@@ -4,3 +4,8 @@ class Cliente:
         self.nome = nome
         self.email = email
 clientes = []
+
+def registrar_cliente(nome, email):
+    cliente = Cliente(nome, email)
+    clientes.append(cliente)
+    print(f"Cliente {nome} registrado.")
