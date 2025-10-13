4f96d394d57fceb2a9b279ba3b2c8e4c6befdbe5
anadornas <analuizadornas94@gmail.com>
2025-10-13 11:26:34 +0100

    Implementa função para listar clientes registrados

--- clientes.py
+++ clientes.py
@@ -9,3 +9,10 @@ def registrar_cliente(nome, email):
     cliente = Cliente(nome, email)
     clientes.append(cliente)
     print(f"Cliente {nome} registrado.")
+
+def listar_clientes():
+    if not clientes:
+        print("Nenhum cliente registrado.")
+    else:
+        for i, cliente in enumerate(clientes, 1):
+            print(f"{i}. {cliente.nome} - {cliente.email}")
