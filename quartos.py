b6e47592d5ffb9ca7f9d439035799b859bdf50e0
anadornas <analuizadornas94@gmail.com>
2025-10-13 13:47:13 +0100

    Implementa função para liberar um quarto

--- quartos.py
+++ quartos.py
@@ -5,3 +5,7 @@ def ocupar_quarto(numero):
     if verificar_disponibilidade(numero):
         quartos[numero]["status"] = "ocupado"
         print(f"Quarto {numero} ocupado.")
+def liberar_quarto(numero):
+    if numero in quartos:
+        quartos[numero]["status"] = "disponivel"
+        print(f"Quarto {numero} liberado.")
