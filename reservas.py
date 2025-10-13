c1d2c3ba4621d6c547b913ae5e6884bec9e8ba2e
anadornas <analuizadornas94@gmail.com>
2025-10-13 13:58:39 +0100

    Implementa função para criar uma nova reserva

--- reservas.py
+++ reservas.py
@@ -1,3 +1,8 @@
 from clientes import Cliente
 from quartos import verificar_disponibilidade, ocupar_quarto
 reservas = []
+def criar_reserva(cliente, numero_quarto):
+    if verificar_disponibilidade(numero_quarto):
+        ocupar_quarto(numero_quarto)
+        reservas.append({"cliente": cliente, "quarto": numero_quarto})
+        print(f"Reserva para {cliente.nome} criada no quarto {numero_quarto}.")
